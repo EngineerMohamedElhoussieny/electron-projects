bbb3ce628fb37350186c5ed717300b37981ac1c2
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-06-08 02:40:53 +0200

    notification-timing

--- Sys-top/app/js/monitor.js
+++ Sys-top/app/js/monitor.js
@@ -5,12 +5,7 @@ const mem=osu.mem
 const os=osu.os
 
 let cpuOverload=5
-
-notifyUser({
-    title: 'CPU Overload',
-    body: `CPU is over ${cpuOverload}%`,
-    icon: path.join(__dirname, 'img', 'icon.png'),
-  })
+let alertFrequency=1
 
 //Run every 2 second
 setInterval(()=>{
@@ -26,6 +21,17 @@ setInterval(()=>{
         }else{
             document.getElementById('cpu-progress').style.background='#30c88b'
         }
+
+       // Check overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+        notifyUser({
+          title: 'CPU Overload',
+          body: `CPU is over ${cpuOverload}%`,
+          icon: path.join(__dirname, 'img', 'icon.png'),
+        })
+  
+        localStorage.setItem('lastNotify', +new Date())
+      }
     })
 
     //CPU Free
@@ -62,7 +68,25 @@ function secondsToDhms(seconds){
 }
 
 // Send notification
-function notifyUser(options) {
-    new Notification(options.title, options)
-  }
+//function notifyUser(options) {
+  //  new Notification(options.title, options)
+ // }
   
+// Check how much time has passed since notification
+function runNotify(frequency) {
+    if (localStorage.getItem('lastNotify') === null) {
+      // Store timestamp
+      localStorage.setItem('lastNotify', +new Date())
+      return true
+    }
+    const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify')))
+    const now = new Date()
+    const diffTime = Math.abs(now - notifyTime)
+    const minutesPassed = Math.ceil(diffTime / (1000 * 60))
+  
+    if (minutesPassed > frequency) {
+      return true
+    } else {
+      return false
+    }
+  }
