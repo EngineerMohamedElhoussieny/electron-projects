729e40a0f61ef23accfc510993358764ca2197bf
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-06-08 23:53:05 +0200

    log-state-table

--- BugLoger/src/components/App.js
+++ BugLoger/src/components/App.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Table from 'react-bootstrap/Table'
+import LogItem from './LogItem'
 
 const App = () => {
+	const [logs,setLogs]=useState([
+		{
+			id:1,
+			text:'This is log one',
+			priority:'low',
+			user:'Brad',
+			created:new Date().toString(),
+		},
+		{
+			id:2,
+			text:'This is log Two',
+			priority:'moderate',
+			user:'Kate',
+			created:new Date().toString(),
+		},{
+			id:3,
+			text:'This is log Three',
+			priority:'high',
+			user:'John',
+			created:new Date().toString(),
+		}
+	])
 	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
+		<Container className='app'>
+			<Table>
+				<thead>
+					<tr>
+						<th>Priority</th>
+						<th>Log Text</th>
+						<th>User</th>
+						<th>Created</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{ logs.map((log,index)=>
+						<LogItem key={index} />
+					)}
+				</tbody>
+			</Table>
+		</Container>
 	)
 }
 
